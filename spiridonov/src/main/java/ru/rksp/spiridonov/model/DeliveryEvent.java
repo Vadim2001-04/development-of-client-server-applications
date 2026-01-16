f629c8761e46d088cb4e2661abb37f7784a35e27
Vadim Spiridonov <vsoiridobv@mail.ru>
2026-01-16 18:06:07 +0300

    Исправления

--- spiridonov/src/main/java/ru/rksp/spiridonov/model/DeliveryEvent.java
+++ spiridonov/src/main/java/ru/rksp/spiridonov/model/DeliveryEvent.java
@@ -1,8 +1,9 @@
 package ru.rksp.spiridonov.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class DeliveryEvent {
+public class DeliveryEvent implements Serializable {
     private String отправитель;
     private String получатель;
     private String адресДоставки;
@@ -13,16 +14,27 @@ public class DeliveryEvent {
 
     public String getОтправитель() { return отправитель; }
     public void setОтправитель(String отправитель) { this.отправитель = отправитель; }
-    
+
     public String getПолучатель() { return получатель; }
     public void setПолучатель(String получатель) { this.получатель = получатель; }
-    
+
     public String getАдресДоставки() { return адресДоставки; }
     public void setАдресДоставки(String адресДоставки) { this.адресДоставки = адресДоставки; }
-    
+
     public String getСтатус() { return статус; }
     public void setСтатус(String статус) { this.статус = статус; }
-    
+
     public LocalDateTime getДатаСобытия() { return датаСобытия; }
     public void setДатаСобытия(LocalDateTime датаСобытия) { this.датаСобытия = датаСобытия; }
+
+    @Override
+    public String toString() {
+        return "DeliveryEvent{" +
+                "отправитель='" + отправитель + '\'' +
+                ", получатель='" + получатель + '\'' +
+                ", адресДоставки='" + адресДоставки + '\'' +
+                ", статус='" + статус + '\'' +
+                ", датаСобытия=" + датаСобытия +
+                '}';
+    }
 }
